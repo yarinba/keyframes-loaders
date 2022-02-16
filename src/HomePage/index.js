c28406f0620ac19017e38d413ed7a79971633f25
YarinRCL <yarinb@rightcodelabs.com>
2022-02-16 13:25:09 +0200

    create 3d loader

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -10,58 +10,62 @@ import Loader7 from '../LoadersComponents/Loader7';
 import Button from '../AtomsComponents/Button';
 
 import './index.scss';
+import Loader8 from '../LoadersComponents/Loader8';
 
-const loaders= [
-    {
-    title: 'Loader 1',
-    component: <Loader1 />
-    },
-    {
-    title: 'Loader 2',
-    component: <Loader2 />
-    },
-    {
-    title: 'Loader 3',
-    component: <Loader3 />
-    },
-    {
-    title: 'Loader 4',
-    component: <Loader4 />
-    },
-    {
-    title: 'Loader 5',
-    component: <Loader5 />
-    },
-    {
-    title: 'Loader 6',
-    component: <Loader6 />
-    },
-        {
-    title: 'Loader 7',
-    component: <Loader7 />
-    },
-]
-
+const loaders = [
+	{
+		title: 'Loader 1',
+		component: <Loader1 />,
+	},
+	{
+		title: 'Loader 2',
+		component: <Loader2 />,
+	},
+	{
+		title: 'Loader 3',
+		component: <Loader3 />,
+	},
+	{
+		title: 'Loader 4',
+		component: <Loader4 />,
+	},
+	{
+		title: 'Loader 5',
+		component: <Loader5 />,
+	},
+	{
+		title: 'Loader 6',
+		component: <Loader6 />,
+	},
+	{
+		title: 'Loader 7',
+		component: <Loader7 />,
+	},
+	{
+		title: 'Loader 8',
+		component: <Loader8 />,
+	},
+];
 
 const HomePage = () => {
-    const [isClicked, setIsClicked] = useState(false);
-    const [displayedLoderIndex, setDisplayedLoaderIndex] = useState(null)
+	const [isClicked, setIsClicked] = useState(false);
+	const [displayedLoderIndex, setDisplayedLoaderIndex] = useState(null);
 
-    const handleClick = (index) => {
-        setIsClicked(true);
-        setDisplayedLoaderIndex(index)
-    }
+	const handleClick = (index) => {
+		setIsClicked(true);
+		setDisplayedLoaderIndex(index);
+	};
 
-    return (
-        <div className="homepage-wrapper">
-            <div className="btns-container">
-                {loaders.map((loader, index) => <Button key={loader.title} loader={loader} onClick={() => handleClick(index)}/>)}
-            </div>
-            <div className="loader-container">
-                {isClicked && loaders[displayedLoderIndex].component}
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="homepage-wrapper">
+			<div className="btns-container">
+				{loaders.map((loader, index) => (
+					<Button key={index} title={loader.title} onClick={() => handleClick(index)} />
+				))}
+			</div>
+			<div className="loader-container">{isClicked && loaders[displayedLoderIndex].component}</div>
+		</div>
+	);
+};
 
-export default HomePage
+export default HomePage;
---
+++ src/LoadersComponents/Loader8/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.scss';
+
+const Loader8 = () => {
+	return (
+		<div className="loader8-container">
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+			<span className="square"></span>
+		</div>
+	);
+};
+
+export default Loader8;
